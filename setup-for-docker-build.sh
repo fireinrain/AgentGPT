518ddd75d8cb76398575fb1d9f54e2ad584f1c9a
sunrise <575563079@qq.com>
2023-04-28 01:29:09 +0800

    update docker-compose-simple.yml

---
+++ setup-for-docker-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+
+OPENAI_API_KEY=""
+NEXTAUTH_SECRET=$(openssl rand -base64 32)
+
+ENV="NODE_ENV=development\n\
+NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
+NEXTAUTH_URL=http://localhost:3000\n\
+OPENAI_API_KEY=$OPENAI_API_KEY\n\
+DATABASE_URL=file:../db/db.sqlite\n"
+
+printf $ENV > .env
+
